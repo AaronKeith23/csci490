4e83d2888c9a6b11b5548c824d6c6920897eda73
AaronKeith23 <54514908+AaronKeith23@users.noreply.github.com>
2019-10-19 21:07:22 -0400

    Removed SQL code from Connection.php

--- connection.php
+++ connection.php
@@ -13,14 +13,6 @@ try{
     $pass = "csci22501fa18!";
     $pdo = new PDO($connString,$user,$pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = "CREATE TABLE workout(
-     id INT(30) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-     name VARCHAR(300) NOT NULL,
-     calorielist INT(300) NOT NULL,
-)";
-    $pdo->exec($sql);
-
 }
 catch (PDOException $e)
 {

e2d6000c95e3086b014aa6c4a3296cad3b248c7f
AaronKeith23 <54514908+AaronKeith23@users.noreply.github.com>
2019-10-19 21:27:36 -0400

    Create legworkout.php

---
+++ legworkout.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: aaronkeith
+ * Date: 9/29/2019
+ * Time: 6:55 PM
+ */
+$pagetitle = "Display Leg Workouts";
+include_once "header.php";
+
+try{
+    //query the data
+    $sql = "SELECT * FROM legworkout";
+    $result = $pdo->query($sql);
+    ?>
+    <?php
+    echo "<table>
+            <h2>User Workout Plan</h2>
+            <tr><th>Workout Name</th><th>Total Reps</th><th>Number of Sets</th></tr>";
+    //loop through the results and display to the screen
+    foreach ($result as $row){
+        echo "<tr><td>{$row['workoutname']}</td><td>" .$row['reps']."</td><td>{$row['sets']}</td><td>"; "</tr>";
+    }
+    echo "</table>";
+}
+catch (PDOException $e)
+{
+    die( $e->getMessage() );
+}
+require_once "footer.php";

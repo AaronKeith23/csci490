3796b7fb7e05ff950ceed9a98e9a93e3425e7d16
AaronKeith23 <54514908+AaronKeith23@users.noreply.github.com>
2019-11-05 12:33:09 -0500

    Update displaychestworkout.php

--- displaychestworkout.php
+++ displaychestworkout.php
@@ -5,7 +5,7 @@
  * Date: 9/29/2019
  * Time: 6:55 PM
  */
-$pagetitle = "Display Chest Workouts";
+$pagetitle = "Display Chest Workout";
 include_once "header.php";
 
 try{
@@ -16,10 +16,10 @@ try{
     <?php
     echo "<table>
             <h2>User Workout Plan</h2>
-            <tr><th>Workout Name</th><th>Total Reps</th><th>Number of Sets</th></tr>";
+            <tr><th>Workout Name</th><th>Total Reps</th><th>Number of Sets</th><th>Calories</th></tr>";
     //loop through the results and display to the screen
     foreach ($result as $row){
-        echo "<tr><td>{$row['workoutname']}</td><td>" .$row['reps']."</td><td>{$row['sets']}</td><td>"; "</tr>";
+        echo "<tr><td>{$row['workoutname']}</td><td>" .$row['reps']."</td><td>" .$row['sets']."</td><td>{$row['calories']}</td><td>"; "</tr>";
     }
     echo "</table>";
 }

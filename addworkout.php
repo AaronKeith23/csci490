7dd59df5eb4100dc74ae8dd88d58a1f73ecfd3ae
zhuozhuoweiwei <55457136+zhuozhuoweiwei@users.noreply.github.com>
2019-11-11 11:03:03 -0500

    Add files via upload

--- addworkout.php
+++ addworkout.php
@@ -1,39 +1,121 @@
+
+
+
+
+<html>
+<head>
+    <title>Add Data</title>
+</head>
+
+<body>
 <?php
+//including the database connection file
+include_once("connection.php");
 require_once 'header.php';
-//check if name/calorielist is empty
-if(!isset($_POST['name'])){
-    die('name is not define');
+$formdata['workoutname'] = trim($_POST['workoutname']);
+$formdata['reps'] = trim($_POST['reps']);
+$formdata['sets'] = trim($_POST['sets']);
+$formdata['calorielist'] = trim($_POST['calorielist']);
+
+if(isset($_POST['Submit'])) {
+    //$workoutname = $_POST['workoutname'];
+   // $reps = $_POST['reps'];
+    //$sets = $_POST['sets'];
+    //$calorielist = $_POST['calorielist'];
+
+    // checking empty fields
+    if(empty($formdata['workoutname']) || empty($formdata['reps']) || empty($formdata['sets'] )||empty($formdata['calorielist'])) {
+
+        if(empty($formdata['workoutname'])) {
+            echo "<span style=\"color: red; \">Name field is empty.</span><br/>";
+        }
+
+        if(empty($formdata['reps'])) {
+            echo "<span style=\"color: red; \">reps field is empty.</span><br/>";
+        }
+
+        if(empty($formdata['sets'])) {
+            echo "<span style=\"color: red; \">sets field is empty.</span><br/>";
+        }
+        if(empty($formdata['calorielist'])) {
+            echo "<span style=\"color: red; \">Calorielist field is empty.</span><br/>";
+        }
+
+        //link to the previous page
+        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+    } else {
+        // if all the fields are filled (not empty)
+
+        //insert data to database
+        $sql = "INSERT INTO workout(workoutname, reps, sets,calorielist) VALUES(:workoutname, :reps, :sets,:calorielist)";
+        $query = $pdo->prepare($sql);
+
+        $query->bindValue(':workoutname', $formdata['workoutname']);
+        $query->bindValue(':reps',$formdata['reps'] );
+        $query->bindValue(':sets', $formdata['sets']);
+        $query->bindValue(':calorielist', $formdata['calorielist']);
+        $query->execute();
+
+
+        //display success message
+        echo "<span style=\"color: green; \">Data added successfully.";
+        echo "<br/><a href='allwork.php'>View Result</a>";
+    }
 }
-if(!isset($_POST['calorielist'])){
-    die('calorielist is not define');
-}
-$name=$_POST['name'];
-$calorielist=$_POST['calorielist'];
-if(empty($name)){
-    die('name is empty');
-}
-if(empty($calorielist)){
-    die('calorielist is empty');
-}
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+//require_once 'header.php';
+#check if name/calorielist is empty
+//if(!isset($_POST['workoutname'])){
+   //die('name is not define');
+//}
+//if(!isset($_POST['calorielist'])){
+    //die('calorielist is not define');
+//}
+
+//if(empty($name)){
+    //die('name is empty');
+//}
+//if(empty($calorielist)){
+    //die('calorielist is empty');
+//}
 //connect to database
 //connnetDb();
 
 
+
 //$calorielist=intval($calorielist);
 //insert data
-try{
-    $pdo->query("INSERT INTO workout(name,calorielist) VALUES ('$name',$calorielist)");
-}
-catch(PDOException $e){
-    die( $e->getMessage() );
-}
-header("Location:allwork.php");
+//$workoutname=$_POST['workoutname'];
+//$reps=$_POST['reps'];
+//$sets=$_POST['sets'];
+//$calorielist=$_POST['calorielist'];
+//try{
+
+        //$sql= "INSERT INTO workout(workoutname,reps,sets,calorielist) VALUES ('".$_POST["workoutname"]."','".$_POST["reps"]."','".$_POST["sets"].",".$_POST["calorielist"]."')";
+        //$stmt = $pdo->prepare($sql);
+        //$stmt ->execute([$workoutname,$reps,$sets,$calorielist]);
+    //}
+    //catch(PDOException $e){
+        //die( $e->getMessage() );
+    //}
+
+//header("Location:allwork.php");
+
 
 
 ?>
 
-<form action="addworkout.php" method="post">
-    <label>WorkoutName：</label><input type="text" name="name">
-    <label>Calorielist：</label><input type="text" name="calorielist">
-    <input type="submit" value="Submit">
-</form>
+
--- allwork.php
+++ allwork.php
@@ -1,50 +1,57 @@
 <?php
+include_once("connection.php");
 require_once 'header.php';
 ?>
-<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>ALL Workouts</title>
-    <style>
-        table{
-            border-collapse: collapse;
-        }
-        th,td{
-            border:1px solid #ccccff;
-            padding: 5px;
-        }
-        td{
-            text-align: center;
-        }
-    </style>
+    <title>Homepage</title>
 </head>
+
 <body>
-<a href="addworkout.php">Add workout</a>
-<table>
-    <tr><th>id</th><th>WorkName</th><th>Calorielist</th><th>Edit/Delete</th></tr>
+<a href="add.html">Add New Data</a><br/><br/>
+
+<table width='80%' border=0>
+
+    <tr bgcolor='#CCCCCC'>
+        <td>Workoutname</td>
+        <td>reps</td>
+        <td>sets</td>
+        <td>Action</td>
+    </tr>
     <?php
     //connect database
     //connnetDb();
     //Query all the data in the data table, and according to the descending order id
-    $result=$pdo->query("SELECT * FROM workout ORDER BY id DESC");
-    $result_count =  $pdo->query("SELECT COUNT(*) FROM workout" );
+    //$result=$pdo->query("SELECT * FROM workout");
+    //$result_count =  $pdo->query("SELECT COUNT(*) FROM workout" );
     //To get the table data row number
-    $result_row = $result_count->fetchColumn();
+    //$result_row = $result_count->fetchColumn();
     //$dataCount=mysql_num_rows($result);
     //echo $dataCount;
     //
+    $result = $pdo->query("SELECT * FROM workout ORDER BY id DESC");
 
-
-    for($i=0;$i<$result_row;$i++){
-        $result_arr=$result->fetch(PDO::FETCH_ASSOC);
-        $id=$result_arr['id'];
-        $name=$result_arr['name'];
-        $age=$result_arr['calorielist'];
-        //print_r($result_arr);
-        echo "<tr><td>$id</td><td>$name</td><td>$calorielist</td><td><a href='editworkout.php?id=$id'>Edit</a> <a href='deleteworkout.php?id=$id'>Delete</a></td></tr>";
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>".$row['workoutname']."</td>";
+        echo "<td>".$row['reps']."</td>";
+        echo "<td>".$row['sets']."</td>";
+        echo "<td>".$row['calorielist']."</td>";
+        echo "<td><a href=\"edit.php?id=$row[id]\">Edit</a> | <a href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
     }
+
+
+    //for($i=0;$i<$result_row;$i++){
+        //$result_arr=$result->fetch(PDO::FETCH_ASSOC);
+        //$id=$result_arr['id'];
+        //$workoutname=$result_arr['workoutname'];
+        //$reps=$result_arr['reps'];
+        //$sets=$result_arr['sets'];
+        //$age=$result_arr['calorielist'];
+        //print_r($result_arr);
+        //echo "<tr><td>$id</td><td>$workoutname</td><td>$reps</td><td>$sets</td><td>$calorielist</td><td><a href='editworkout.php?id=$id'>Edit</a> <a href='deleteworkout.php?id=$id'>Delete</a></td></tr>";
+    //}
     ?>
-</table>
+    </table>
 </body>
 </html>
